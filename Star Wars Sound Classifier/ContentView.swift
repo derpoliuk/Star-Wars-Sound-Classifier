e3aa4a4cb3a5ffbc3db09370bb428b94d488c5aa
Stanislav Derpoliuk <derpoliuk@gmail.com>
2023-08-04 23:35:55 -0300

    Initial Commit

---
+++ Star Wars Sound Classifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Star Wars Sound Classifier
+//
+//  Created by Stanislav Derpoliuk on 2023-08-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Star Wars Sound Classifier/Star_Wars_Sound_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_Wars_Sound_ClassifierApp.swift
+//  Star Wars Sound Classifier
+//
+//  Created by Stanislav Derpoliuk on 2023-08-04.
+//
+
+import SwiftUI
+
+@main
+struct Star_Wars_Sound_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
